24587f283c17ee20da30be7e4f035476a23b608a
eliasdopontiac <114600873+eliasdopontiac@users.noreply.github.com>
2026-05-02 15:05:46 -0400

    atualizei

--- Telemetria_2026/Fardriver_pro/main.py
+++ Telemetria_2026/Fardriver_pro/main.py
@@ -1,8 +1,10 @@
 import sys
+
 from PyQt5.QtWidgets import QApplication
 
-# Importa a Interface Gráfica que criamos
+# Importa a Interface Gráfica que criamos ------
 from ui_dashboard import FardriverApp
+
 
 def main():
     """
@@ -10,16 +12,17 @@ def main():
     Este é o arquivo que deve ser executado no terminal.
     """
     print("A inicializar o Fardriver Pro - Bancada de Testes...")
-    
+
     # Inicializa o motor do PyQt5
     app = QApplication(sys.argv)
-     
+
     # Cria a janela principal
     janela = FardriverApp()
     janela.show()
-    
+
     # Arranca o loop principal da aplicação e mantém a janela aberta
     sys.exit(app.exec_())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
